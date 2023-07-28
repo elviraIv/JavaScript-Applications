7b462ae8a2831dab9f28bec0ce35405f517f6026
Elvira <elvira.n.ivanova@gmail.com>
2023-07-28 14:13:29 +0300

    Added an exercise

--- asynchronous-programming/Blog/app.js
+++ asynchronous-programming/Blog/app.js
@@ -1,5 +1,81 @@
-function attachEvents() {
-    console.log('TODO...');
-}
+function attachEvents() {}
+const loadPostsBtn = document.getElementById("btnLoadPosts");
+const viewPostsBtn = document.getElementById("btnViewPost");
+
+loadPostsBtn.addEventListener("click", getAllPosts);
+viewPostsBtn.addEventListener("click", displayPost);
 
 attachEvents();
+
+async function displayPost() {
+  //get selected value from options
+  //display post
+  //display comments for the post
+
+  const selectedId = document.getElementById("posts").value;
+  const post = await getPostById(selectedId);
+  const comments = await getCommentsByPostId(selectedId);
+
+  const postTitleElement = document.getElementById("post-title");
+  const liPostBodyElement = document.getElementById("post-body");
+  const ulCommentsElement = document.getElementById("post-comments");
+
+  postTitleElement.textContent = post.title;
+  liPostBodyElement.textContent = post.body;
+  ulCommentsElement.replaceChildren();
+  comments.forEach((c) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = c.text;
+    ulCommentsElement.appendChild(liElement);
+  });
+}
+
+async function getAllPosts() {
+  try {
+    const url = `http://localhost:3030/jsonstore/blog/posts`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    //populating slect->options(list)
+    const selectElement = document.getElementById("posts");
+    loadPostsBtn.disabled = true;
+
+    Object.values(data).forEach((p) => {
+      const optionElement = document.createElement("option");
+      optionElement.textContent = p.title;
+      optionElement.value = p.id;
+
+      selectElement.appendChild(optionElement);
+    });
+  } catch (error) {
+    throw new Error("Error");
+  }
+}
+
+async function getPostById(postId) {
+  try {
+    const url = `http://localhost:3030/jsonstore/blog/posts/${postId}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+async function getCommentsByPostId(postId) {
+  try {
+    const url = `http://localhost:3030/jsonstore/blog/comments`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const comments = Object.values(data).filter((c) => c.postId === postId);
+
+    return comments;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
